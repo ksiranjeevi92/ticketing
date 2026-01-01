06b49111a9fdd6241acd33aeab07f049ae6ad048
siranjeevi <ksiranjeevi92@gmail.com>
2026-01-01 13:35:37 +0530

    Created tickets

---
+++ tickets/src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+import { NotFoundError, errorHandler } from "@byteurl.com/common";
+
+const app = express();
+
+app.use(express.json());
+
+app.set("trust proxy", true);
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
